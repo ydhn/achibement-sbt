16cc99e7a8e72f948ca7b9906eaf9919101c8a85
Jordan <han@yun.do>
2022-12-18 06:31:11 +0900

    fix: avoid reuse signature

--- contracts/Achiebement.sol
+++ contracts/Achiebement.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
@@ -16,25 +18,25 @@ contract Achiebement is ERC1155, ERC1155Burnable, Ownable, ERC5633 {
         _signer = signer;
     }
 
-    function recover(uint256[] calldata ids, uint256[] calldata amounts, bytes memory _signature)
+    function recover(uint256[] calldata ids, uint256[] calldata amounts, uint256[] calldata holdings, bytes memory _signature)
     private
     view
     returns (address)
     {
-        return ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256(abi.encode(msg.sender, ids, amounts))), _signature);
+        return ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256(abi.encode(msg.sender, ids, amounts, holdings))), _signature);
     }
 
-    function mint(uint256[] calldata ids, uint256[] calldata amounts, bytes memory _signature)
+    function mint(uint256[] calldata ids, uint256[] calldata amounts, uint256[] calldata holdings, bytes memory _signature)
     external
     {
         require(ids.length == amounts.length, "Invalid arguments");
-        require(_signer == recover(ids, amounts, _signature), "Invalid Signature");
-
-        _mintBatch(msg.sender, ids, amounts, "");
+        require(_signer == recover(ids, amounts, holdings, _signature), "Invalid signature");
         for (uint256 i = 0; i < ids.length; i++) {
             uint256 id = ids[i];
+            require(balanceOf(msg.sender, id) == holdings[i], "Signature expired");
             if (!isSoulbound(id)) _setSoulbound(id, true);
         }
+        _mintBatch(msg.sender, ids, amounts, "");
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
